a9267762ce0227daa819415d65accfb1a940276f
Sean McKinney <seanamckinney8@gmail.com>
2022-05-16 11:05:35 -0500

    init commit:  Created a App with previous interface IClown and added IScaryClown interface along with Classes FunnyFunny and ScaryScar.  Had to construct methods and satisfy contract for interfaces and then fix book statement in Main to cast fingersTheClown to ScaryScary via IScaryClown using "is" keyword

--- IClownInterfaceExerciseHFCSPg389/Program.cs
+++ IClownInterfaceExerciseHFCSPg389/Program.cs
@@ -6,10 +6,64 @@ using System.Threading.Tasks;
 
 namespace IClownInterfaceExerciseHFCSPg389
 {
+    //interfaces
+    interface IClown
+    {
+        string FunnyThingIHave { get; }
+        void Honk();
+    }//end IClown
+
+    interface IScaryClown : IClown
+    {
+        string ScaryThingIHave { get; }
+        void ScareLittleChildren();
+    }//end IScaryClown
+
+    //classes
+
+    class FunnyFunny : IClown
+    {
+        private string funnyThingIHave;
+        public string FunnyThingIHave { get { return funnyThingIHave; } }
+
+        public FunnyFunny(string funnyThingIHave)
+        {
+            this.funnyThingIHave = funnyThingIHave;
+        }
+
+        public void Honk()
+        {
+            Console.WriteLine($"Hi kids! I have a {funnyThingIHave}.");
+        }
+    }//end class
+
+    class ScaryScary : FunnyFunny, IScaryClown
+    {
+        private int scaryThingCount;
+        public ScaryScary(string funnyThing, int scaryThingCount) : base(funnyThing)
+        {
+            this.scaryThingCount = scaryThingCount;
+        }
+
+        public string ScaryThingIHave { get { return $"{scaryThingCount} spiders"; } }
+
+        public void ScareLittleChildren()
+        {
+            Console.WriteLine($"Boo! Gotcha! Look at my {ScaryThingIHave}!");
+        }
+
+    }//end class
+
     class Program
     {
         static void Main(string[] args)
         {
-        }
-    }
-}
+            IClown fingersTheClown = new ScaryScary("big red nose", 14);
+            fingersTheClown.Honk();
+            if (fingersTheClown is IScaryClown iScaryClownReference)
+            {
+                iScaryClownReference.ScareLittleChildren();
+            }
+        }//end main
+    }//end class
+}//end namespace
